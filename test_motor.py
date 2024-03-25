a083c8c02a180ed1933f3a51e3d1b3f5ed37dddc
vincentverges <me@vincentverges.com>
2024-03-25 13:49:40 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -6,37 +6,38 @@ pygame.init()
 screen = pygame.display.set_mode((100, 100))
 
 motor3_pwm = Servo("P3")
-
-angle_motor = 0
-motor3_pwm.angle(angle_motor)
+angle_motor = 0  # Angle initial du moteur
 
 def adjust_motor_speed(new_angle_motor):
     motor3_pwm.angle(new_angle_motor)
     print(f"Motor Pulse Width: {new_angle_motor}")
 
 running = True
-print("Utiliser les flèches 'Haut' et 'Bas' pour contrôler le moteur")
+print("Maintenir la flèche 'Haut' pour accélérer le moteur")
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
-    keys = pygame.key.get_pressed()
-    motor_changed = False
-    
-    while keys[pygame.K_UP]:
-        angle_motor = min(90, angle_motor + 1) 
-        motor_changed = True
-        adjust_motor_speed(angle_motor)
 
-    while keys[pygame.K_DOWN]:
-        angle_motor = max(-90, angle_motor - 1)
-        motor_changed = True
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_UP]:
+        angle_motor += 1  # Augmenter l'angle pour accélérer
+        angle_motor = min(angle_motor, 90)  # Limiter l'angle maximal à 90
+        adjust_motor_speed(angle_motor)
+    elif keys[pygame.K_DOWN]:
+        angle_motor -= 1  # Diminuer l'angle pour ralentir
+        angle_motor = max(angle_motor, -180)  # Limiter l'angle minimal à -180
         adjust_motor_speed(angle_motor)
     else:
-        angle_motor = 0
+        # Optionnel : arrêter progressivement le moteur si aucune touche n'est pressée
+        if angle_motor > 0:
+            angle_motor -= 1
+        elif angle_motor < 0:
+            angle_motor += 1
         adjust_motor_speed(angle_motor)
-    
-    time.sleep(0.0001)
-        
+
+    time.sleep(0.01)  # Un petit délai pour contrôler la vitesse de changement d'angle et la réactivité
+
 pygame.quit()
