d1564bc24ffd20cac7a6be8969fe4d5e67b78abd
vincentverges <me@vincentverges.com>
2024-03-25 13:12:17 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -2,13 +2,10 @@ import pygame
 from robot_hat import Servo, PWM, TTS
 import time
 
-pygame.init()
-screen = pygame.display.set_mode((100,100))
-
 motor = PWM('P1')
-motor.freq(1000)
-
-motor.pulse_width_percent(100)
+motor.pulse_width(2500)
+time.sleep(4)
+motor.pulse_width(1500)
 
 
 
