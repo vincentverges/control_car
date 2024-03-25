9054f80c0d7517636535b28eb246067d8f243f0e
vincentverges <me@vincentverges.com>
2024-03-25 14:34:33 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -1,15 +1,14 @@
-import robot_hat
 from robot_hat import PWM
 import time
 
 # Initialisation
-pwm_motor = PWM("P3")  # Assumer que "P0" est le canal pour le moteur
+pwm_motor = PWM("P1")  # Assumer que "P0" est le canal pour le moteur
 
 # Configurer la fréquence PWM pour le servo et le moteur
 pwm_motor.freq(60)  # La fréquence peut devoir être ajustée selon vos composants
 
 # Valeurs initiales
-motor_speed = 1500  # Valeur neutre pour l'ESC
+motor_speed = 400  # Valeur neutre pour l'ESC
 
 def update_motor(speed):
     pwm_motor.pulse_width(speed)
@@ -28,5 +27,5 @@ try:
         
 except KeyboardInterrupt:
     # Arrêt propre du programme
-    pwm_motor.pulse_width(1500)  # Envoyer un signal neutre à l'ESC pour arrêter le moteur
+    pwm_motor.pulse_width(400)  # Envoyer un signal neutre à l'ESC pour arrêter le moteur
     print("Arrêt du programme.")
