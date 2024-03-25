4ada6bba7a2bfc10ea725de703858062707a52a5
vincentverges <me@vincentverges.com>
2024-03-25 14:00:33 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -12,44 +12,36 @@ def adjust_motor_speed(new_angle_motor):
     motor3_pwm.angle(new_angle_motor)
     print(f"Motor Pulse Width: {new_angle_motor}")
 
-def get_key_state():
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_UP]:
-        return "UP"
-    elif keys[pygame.K_DOWN]:
-        return "DOWN"
-    else:
-        return "NONE"
-
 running = True
 print("Maintenir la flèche 'Haut' pour accélérer le moteur")
 
-last_key_state = "NONE"
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    key_state = get_key_state()
+    keys = pygame.key.get_pressed()
+    key_pressed = False  # Flag pour indiquer si une touche est pressée
 
-    if key_state == "UP":
-        if angle_motor < 90:
-            angle_motor += 1  # Augmente progressivement jusqu'à 90
-    elif key_state == "DOWN":
-        if angle_motor > -90:
-            angle_motor -= 1  # Diminue progressivement jusqu'à -90
-    else:
-        # Si aucune touche n'est pressée, réduire progressivement l'angle à 0
+    if keys[pygame.K_UP]:
+        key_pressed = True
+        if angle_motor < 80:  # Augmenter l'angle seulement s'il est inférieur à 90
+            angle_motor += 1
+        adjust_motor_speed(angle_motor)  # Maintenir la vitesse maximale si angle_motor est à 90
+    elif keys[pygame.K_DOWN]:
+        key_pressed = True
+        if angle_motor > -80:  # Diminuer l'angle seulement s'il est supérieur à -90
+            angle_motor -= 1
+        adjust_motor_speed(angle_motor)
+
+    if not key_pressed:
+        # Arrêter progressivement le moteur si aucune touche n'est pressée
         if angle_motor > 0:
             angle_motor -= 1
         elif angle_motor < 0:
             angle_motor += 1
+        adjust_motor_speed(angle_motor)
 
-    if key_state != last_key_state or key_state == "NONE":
-        adjust_motor_speed(angle_motor)  # Ajuste la vitesse du moteur
-
-    last_key_state = key_state  # Mémorise l'état de la touche pour la prochaine itération
-
-    time.sleep(0.01)  # Délai pour une mise à jour fluide
+    time.sleep(0.01)  # Un petit délai pour contrôler la vitesse de changement d'angle et la réactivité
 
 pygame.quit()
