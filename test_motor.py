a29caa5f3f8084f00bd8fe58f9bcc6e03b02d951
vincentverges <me@vincentverges.com>
2024-03-25 13:36:40 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -3,36 +3,36 @@ from robot_hat import Servo
 import time
 
 pygame.init()
-screen = pygame.display.set_mode((100,100))
+screen = pygame.display.set_mode((100, 100))
 
 motor3_pwm = Servo("P3")
 
 angle_motor = 0
 motor3_pwm.angle(angle_motor)
-	
+
 def adjust_motor_speed(new_angle_motor):
-	motor3_pwm.angle(new_angle_motor)
-	print(f"Motor Pulse Width: {new_angle_motor}")	
-	return new_angle_motor
+    motor3_pwm.angle(new_angle_motor)
+    print(f"Motor Pulse Width: {new_angle_motor}")
+    return new_angle_motor
 
 running = True
-print("Utiliser les fleches gauche et droite pour controler le Servo")
+print("Utiliser les flèches 'Haut' et 'Bas' pour contrôler le moteur")
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
-			
-	keys = pygame.key.get_pressed()
-	
-	if keys[pygame.K_UP]:
-		angle_motor = min(90, angle_motor + 1)
-	elif keys[pygame.K_DOWN]:
-		angle_motor = max(-180, angle_motor - 1)
-	else:
-		angle_motor = 0
-		
-    adjust_motor_speed(angle_motor)
-	
-	time.sleep(0.001)
-		
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_UP]:
+        angle_motor = min(90, angle_motor + 1)  # Limite supérieure à 90
+    elif keys[pygame.K_DOWN]:
+        angle_motor = max(-180, angle_motor - 1)  # Limite inférieure à -180
+    else:
+        angle_motor = 0
+    
+    adjust_motor_speed(angle_motor)  # Cette ligne doit être alignée avec les blocs if/elif/else
+    
+    time.sleep(0.001)
+        
 pygame.quit()
