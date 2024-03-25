13ae5e0a64fd8b37585a20b5fc355f7cdb38bbf6
vincentverges <me@vincentverges.com>
2024-03-25 14:02:43 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -1,47 +1,18 @@
-import pygame
-from robot_hat import Servo
+from robot_hat import PWM
 import time
 
-pygame.init()
-screen = pygame.display.set_mode((100, 100))
+# Initialisation de l'objet PWM pour le moteur
+motor_pwm = PWM("P3")  # Assurez-vous que "P3" est le port correct
 
-motor3_pwm = Servo("P3")
-angle_motor = 0  # Angle initial du moteur
+# Réglage de la fréquence du signal PWM, qui est commune pour les contrôles de moteurs à courant continu
+motor_pwm.freq(1000)  # Exemple: 1000 Hz
 
-def adjust_motor_speed(new_angle_motor):
-    motor3_pwm.angle(new_angle_motor)
-    print(f"Motor Pulse Width: {new_angle_motor}")
+def set_motor_speed(pulse_width):
+    # Réglage de la largeur d'impulsion pour contrôler la vitesse du moteur
+    # La valeur doit être ajustée en fonction des spécifications de votre moteur et du contrôleur
+    motor_pwm.pulse_width(pulse_width)
 
-running = True
-print("Maintenir la flèche 'Haut' pour accélérer le moteur")
-
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    keys = pygame.key.get_pressed()
-    key_pressed = False  # Flag pour indiquer si une touche est pressée
-
-    if keys[pygame.K_UP]:
-        key_pressed = True
-        if angle_motor < 80:  # Augmenter l'angle seulement s'il est inférieur à 90
-            angle_motor += 1
-        adjust_motor_speed(angle_motor)  # Maintenir la vitesse maximale si angle_motor est à 90
-    elif keys[pygame.K_DOWN]:
-        key_pressed = True
-        if angle_motor > -80:  # Diminuer l'angle seulement s'il est supérieur à -90
-            angle_motor -= 1
-        adjust_motor_speed(angle_motor)
-
-    if not key_pressed:
-        # Arrêter progressivement le moteur si aucune touche n'est pressée
-        if angle_motor > 0:
-            angle_motor -= 1
-        elif angle_motor < 0:
-            angle_motor += 1
-        adjust_motor_speed(angle_motor)
-
-    time.sleep(0.01)  # Un petit délai pour contrôler la vitesse de changement d'angle et la réactivité
-
-pygame.quit()
+# Exemple d'utilisation pour faire tourner le moteur à une vitesse définie par la largeur d'impulsion
+set_motor_speed(1500)  # Exemple de valeur, à ajuster
+time.sleep(4)  # Faire tourner le moteur pendant 4 secondes
+set_motor_speed(0)  # Arrêter le moteur
