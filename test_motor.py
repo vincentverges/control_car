ff40116ef6069fbaf9986a5b72bbb0a9059f2c5a
vincentverges <me@vincentverges.com>
2024-03-25 13:14:43 +0100

    update control

--- test_motor.py
+++ test_motor.py
@@ -2,10 +2,20 @@ import pygame
 from robot_hat import Servo, PWM, TTS
 import time
 
-motor = PWM('P1')
-motor.pulse_width(2500)
-time.sleep(4)
-motor.pulse_width(1500)
+motor = Servo('P1')
+#motor.pulse_width(2500)
+#time.sleep(4)
+#motor.pulse_width(1500)
+motor.angle(0)
+time.sleep(1)
+motor.angle(90)
+time.sleep(3)
+motor.angle(0)
+time.sleep(1)
+motor.angle(0)
+time.sleep(1)
+motor.angle(-90)
+time.sleep(3)
+motor.angle(0)
 
-
-
+print("Test fini")
